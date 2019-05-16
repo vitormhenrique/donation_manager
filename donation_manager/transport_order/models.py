6dbd534c8fd3232bfa3fa440058f7bcfdb647496
djunior3 <djunior3@gatech.edu>
2019-05-15 20:55:43 -0500

    Available_times split in week_day and Time_window

--- donation_manager/transport_order/models.py
+++ donation_manager/transport_order/models.py
@@ -4,6 +4,7 @@ from address.models import AddressField
 from donation_manager.users.models import User
 from donation_manager.donation.models import Donation
 
+
 # Create your models here.
 
 class Transport_Order(models.Model):
@@ -30,10 +31,11 @@ class Transport_Order(models.Model):
         max_length=4,
         choices=TRANSIT_STATUS_CHOICES
     )
-    agreed_time = models.ForeignKey(Donation, on_delete=models.SET_NULL, blank=True, null=True)
+    agreed_weekday = models.OneToOneField("Week_Day", on_delete=models.SET_NULL, blank=True, null=True)
+    agreed_time_window = models.OneToOneField("Time_Window", on_delete=models.SET_NULL, blank=True, null=True)
     
 
-class Available_Times(models.Model):
+class Week_Day(models.Model):
 
     MO = 'MO'
     TU = 'TU'
@@ -43,13 +45,6 @@ class Available_Times(models.Model):
     SA = 'SA'
     SU = 'SU'
 
-    AM_6_to_8 = "06:00 AM - 08:00 AM"
-    AM_8_to_10 = "08:00 AM - 10:00 AM"
-    AM_10_to_Noon = "10:00 AM - Noon"
-    PM_02_to_04 = "02:00 PM - 04:00 PM"
-    PM_04_to_06 = "04:00 PM - 06:00 PM"
-    PM_06_to_08 = "06:00 PM - 08:00 PM"
-
     DAYS_OF_WEEK = (
         (MO, 'Monday'),
         (TU, 'Tuesday'),
@@ -57,8 +52,24 @@ class Available_Times(models.Model):
         (TH, 'Thursday'),
         (FR, 'Friday'),
         (SA, 'Saturday'),
-        (SU, 'Sunday' )
+        (SU, 'Sunday')
     )
+
+    transport_order = models.ForeignKey(Transport_Order, on_delete=models.CASCADE)
+    day_of_week = models.CharField(
+        max_length=2,
+        choices=DAYS_OF_WEEK
+    )
+
+
+class Time_Window(models.Model):
+
+    AM_6_to_8 = "06:00 AM - 08:00 AM"
+    AM_8_to_10 = "08:00 AM - 10:00 AM"
+    AM_10_to_Noon = "10:00 AM - Noon"
+    PM_02_to_04 = "02:00 PM - 04:00 PM"
+    PM_04_to_06 = "04:00 PM - 06:00 PM"
+    PM_06_to_08 = "06:00 PM - 08:00 PM"    
 
     TIME_WINDOWS = (
         (AM_6_to_8, "06:00 AM - 08:00 AM"),
@@ -69,11 +80,8 @@ class Available_Times(models.Model):
         (PM_06_to_08, "06:00 PM - 08:00 PM"),
     )
 
-    transport_order = models.ForeignKey(Transport_Order, on_delete=models.CASCADE)
-    day_of_week = models.CharField(
-        max_length=2,
-        choices=DAYS_OF_WEEK
-    )
+    week_day = models.ForeignKey(Week_Day, on_delete=models.CASCADE)
+
     time_window = models.CharField(
         max_length=20,
         choices=TIME_WINDOWS
