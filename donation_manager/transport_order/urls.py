d9ba8a588537d679cc3e5c365f19edbd0ae96a8d
Diego Volpato <dgvlpt@gmail.com>
2019-05-18 11:56:58 -0500

    url change

--- donation_manager/transport_order/urls.py
+++ donation_manager/transport_order/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     # path("~redirect/", view=user_redirect_view, name="redirect"),
     # path("~update/", view=user_update_view, name="update"),
     # path("<str:username>/", view=user_detail_view, name="detail"),
+    
 ]
